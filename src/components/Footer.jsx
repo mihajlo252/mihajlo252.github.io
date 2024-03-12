3769dc8627f0c35de1c36d3ecc6c3941266bce80
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-03-12 18:31:47 +0100

    Add transitions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,17 +6,17 @@ export const Footer = () => {
 		<div className="footer">
 			<footer className="text-white-content footer footer-center bg-base-200 p-10">
 				<div>
-					<div className="flex items-center gap-4">
-						<a href="https://github.com/mihajlo252" aria-label="Github Link">
-							<BsGithub size={30} />
+					<div className="flex items-center gap-4 text-slate-500">
+						<a className="transition-colors duration-[250ms] hover:text-[#eaeaea]" href="https://github.com/mihajlo252" aria-label="Github Link">
+							<BsGithub size={30} color={""}/>
 						</a>
-						<a href="https://www.linkedin.com/in/mihajlo-kostic/" aria-label="LinedIn Link">
-							<BsLinkedin size={30} />
+						<a className="transition-colors duration-[250ms] hover:text-[#eaeaea]" href="https://www.linkedin.com/in/mihajlo-kostic/" aria-label="LinedIn Link">
+							<BsLinkedin size={30} color="bg-primary"/>
 						</a>
 					</div>
 				</div>
 				<div>
-          <p className="font-bold">All rights reserved © Mihajlo Kostić 2023</p>
+          <p className="font-bold text-slate-500">All rights reserved © Mihajlo Kostić 2023</p>
         </div>
 			</footer>
 		</div>
